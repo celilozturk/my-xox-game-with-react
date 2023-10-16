b1b80a634aeb4ecc73551b873ae1ac1c17be9d66
Celil Ozturk <celil.ozturk@windowslive.com>
2023-10-16 17:01:09 +0300

    Isaretleme ve Kontrol Kodlari

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+function XoxGameComponent() {
+  const [games, setGames] = useState([]);
+  const [mark, setMark] = useState("X");
+  const [message, setMessage] = useState("");
+  const [isGameFinish, setIsGameFinish] = useState(false);
+
+  useEffect(() => {
+    newGame();
+  }, []);
+
+  const newGame = () => {
+    setGames([
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ]);
+    setIsGameFinish(false);
+    setMark("X");
+    setMessage("Hame Sirasi: " + mark);
+  }
+
+  const markGame = (index) => {
+    if(!isGameFinish){
+
+
+    const newGames = [...games];
+    if (newGames[index] == "") {
+      newGames[index] = mark;
+      setGames(newGames);
+      mark == "X" ? setMark("0") : setMark("X");
+      setMessage("Hamle Sirasi: " + (mark == "X" ? "O" : "X"));
+    }
+  }
+  }
+
+  return (
+    <>
+      <div className='container text-center'>
+
+        <h2 className='alert alert-warning'>XOX Oyunu</h2>
+        {message}
+        <button onClick={newGame} className='btn btn-outline-primary w-100'>Yeni Oyun</button>
+        <div className='row mt-2'>
+          {games.map((game, index) => (
+            <div key={index} className='col-md-4 box' onClick={() => markGame(index)}>
+              {game}
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<XoxGameComponent />);
+
+
 reportWebVitals();
